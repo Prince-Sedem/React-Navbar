a9c739684977005832ea890d13940e5b20e25f91
Prince-Sedem <sedemdev@gmail.com>
2024-05-14 17:36:18 +0100

    Add two image files

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Homepage from "./Homepage";
 import Contact from "./Contact";
 import AboutLittleLemon from "./AboutLittleLemon";
+import noodles from "./assets/noodles.png";
 import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
@@ -17,6 +18,8 @@ function App() {
         <Route path="/about" element={<AboutLittleLemon />}></Route>
         <Route path="/contact" element={<Contact />}></Route>
       </Routes>
+      <img src ={noodles} alt= "A picture of a plate of noodles"/>
+      <img src ={require('./assets/Burger.png')} alt= "A picture of a burger"/>
     </div>
   );
 }
