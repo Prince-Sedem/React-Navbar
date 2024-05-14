b15ba7b8e3a45248d599ff0e294a1a5bea26ce9c
Prince-Sedem <sedemdev@gmail.com>
2024-05-14 18:30:39 +0100

    Add a video

--- src/App.js
+++ src/App.js
@@ -4,8 +4,11 @@ import Contact from "./Contact";
 import AboutLittleLemon from "./AboutLittleLemon";
 import noodles from "./assets/noodles.png";
 import { Routes, Route, Link } from "react-router-dom";
+import ReactPlayer from "react-player";
 
 function App() {
+  const vidUrl = 'https://www.youtube.com/watch?v=YN2H_sKcmGw';
+
   return (
     <div> 
 	  <nav>
@@ -20,6 +23,11 @@ function App() {
       </Routes>
       <img src ={noodles} alt= "A picture of a plate of noodles"/>
       <img src ={require('./assets/Burger.png')} alt= "A picture of a burger"/>
+      <ReactPlayer 
+        url={vidUrl}
+        playing={false}
+        volume={0.5}
+      />
     </div>
   );
 }
